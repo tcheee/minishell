19e1e38723799f914a77c81af97b52ce4d9a0ad6
Thomas CHERRET <tcherret@e3r13p5.42.fr>
2019-11-17 18:01:24 +0100

    new

--- srcs/ft_analyze_input.c
+++ srcs/ft_analyze_input.c
@@ -6,11 +6,13 @@ int		exec_command(char *command, char **words)
 	char *PATH;
 	extern char **environ;
 
+	//check_builtin function
 	PATH = ft_strcat("/bin/", command); // to free
 	if (execve(PATH, words, environ) == -1) // need to set up the environ variable
 	{
 		ft_putstr("error\n");
 	};
+	free(PATH);
 	return (0);
 	
 }
@@ -25,12 +27,14 @@ int     ft_analyse_input(char *buff)
 	i = 0;
 	words = ft_strsplit(buff, ' '); // to free
 	command = words[0];
+	ft_printf("The command is : %s\n", command);
+	exec_command(command, words);
 	while (words[i] != NULL)
 	{
 		ft_printf("%s\n", words[i]);
+		free(words[i]);
 		i++;
 	}
-	ft_printf("The command is : %s\n", command);
-	exec_command(command, words);
+	free(words);
 	return (0);
 }
